b6248b0b859c754f19b602b4782c349964099d13
Henry Paredes <henryalexander24@gmail.com>
2024-07-14 13:15:27 -0400

    Step 3 done

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 const app = Vue.createApp({
   data() {
     return{
-      step: 0,
+      step: 2,
+    }
+  },
+  methods: {
+    checked(event){
+      if (event.currentTarget.checked){
+        event.currentTarget.closest('.add-on').classList.add("check-selected")
+      } else{
+        event.currentTarget.closest('.add-on').classList.remove("check-selected")
+      }
     }
   }
+});
+
+const checkboxes = document.querySelectorAll('.add-on input[type="checkbox"]');
+
+function checkboxChecked(e){
+  console.log('checked',)
+
+}
+
+checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('click', function(e) {
+    console.log('checked')
+    if (checkbox.checked){
+      checkbox.closest(".add-on").classList.add("check-selected")
+    } else{
+      checkbox.closest(".add-on").classList.remove("check-selected")
+    }
+  })
 })
