27988d4a88cf553166c6a066afed563b8985879a
Henry Paredes <henryalexander24@gmail.com>
2024-07-15 19:54:14 -0400

    add radio buttons instead of cards

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+
 const app = Vue.createApp({
   data() {
     return{
       step: 0,
+      name: '',
+      email: '',
+      phone: null
     }
   },
   methods: {
@@ -14,21 +18,3 @@ const app = Vue.createApp({
     }
   }
 });
-
-const checkboxes = document.querySelectorAll('.add-on input[type="checkbox"]');
-
-function checkboxChecked(e){
-  console.log('checked',)
-
-}
-
-checkboxes.forEach(function(checkbox) {
-  checkbox.addEventListener('click', function(e) {
-    console.log('checked')
-    if (checkbox.checked){
-      checkbox.closest(".add-on").classList.add("check-selected")
-    } else{
-      checkbox.closest(".add-on").classList.remove("check-selected")
-    }
-  })
-})
