2f1ada325947678182a165799207a10a4581f27b
Henry Paredes <henryalexander24@gmail.com>
2024-07-24 19:52:28 -0400

    cleaning out template

--- components/formDisplay.js
+++ components/formDisplay.js
@@ -83,38 +83,6 @@ app.component('form-display', {
             <div class="add-on-price">{{ getPriceOf(addOn) }}</div>
           </label>
         </template>
-          <!-- <label for="online" class="add-on flex">
-            <div class="add-on-group flex">
-              <div class="add-on-check">
-                <input type="checkbox" @click="checked" name="add-on" id="online">
-              </div>
-              <div class="add-on-description">
-                <h2 class="plan">Online service</h2>
-                <p class="add-on-description">Access to multiplayer games</p>
-              </div>
-            </div>
-            <div class="add-on-price">+$1/mo</div>
-          </label>
-          <label for="storage" class="add-on">
-            <div class="add-on-group">
-              <div class="add-on-check"><input type="checkbox" @click="checked" name="add-on" id="storage"></div>
-              <div class="add-on-description">
-                <h2 class="plan">Larger storage</h2>
-                <p class="add-on-description">Extra 1TB of cloud save</p>
-              </div>
-            </div>
-            <div class="add-on-price">+$2/mo</div>
-          </label>
-          <label for="custom" class="add-on">
-            <div class="add-on-group">
-              <div class="add-on-check"><input type="checkbox" @click="checked" name="add-on" id="custom"></div>
-              <div class="add-on-description">
-                <h2 class="plan">Customizable Profile</h2>
-                <p class="add-on-description">Custom theme on your profile</p>
-              </div>
-            </div>
-            <div class="add-on-price">+$2/mo</div>
-          </label> -->
         </section>
       </section>
       <!-- Step 3 ends -->
